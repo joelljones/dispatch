d9b54031218e1dd16b4042bb050b61115071161a
Joell Jones <25757151+joelljones@users.noreply.github.com>
2023-08-15 16:43:04 -0400

    styled faq accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,46 +1,41 @@
 import { Disclosure } from '@headlessui/react';
 import { ChevronUpIcon } from '@heroicons/react/20/solid';
 
-// TODO: style disclosure/accordion
 // TODO: add faqs
 
 export default function Accordion() {
   return (
     <div className="w-full px-4">
-      <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+      <div className="mx-auto w-full max-w-md rounded-2xl p-2">
         <Disclosure>
           {({ open }) => (
-            <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+            <div className="border-b-[1px] border-[#27272a]">
+              <Disclosure.Button className="flex w-full justify-between py-4 text-left text-base font-medium hover:underline">
                 <span>What is your refund policy?</span>
                 <ChevronUpIcon
-                  className={`${
-                    open ? 'rotate-180 transform' : ''
-                  } h-5 w-5 text-purple-500`}
+                  className={`${open ? 'rotate-180 transform' : ''} h-5 w-5`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+              <Disclosure.Panel className="pb-4 text-sm text-left">
                 If you're unhappy with your purchase for any reason, email us
                 within 90 days and we'll refund you in full, no questions asked.
               </Disclosure.Panel>
-            </>
+            </div>
           )}
         </Disclosure>
-        <Disclosure as="div" className="mt-2">
+        <Disclosure>
           {({ open }) => (
-            <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+            <div className="border-b-[1px] border-[#27272a]">
+              <Disclosure.Button className="flex w-full justify-between py-4 text-left text-base font-medium hover:underline">
                 <span>Do you offer technical support?</span>
                 <ChevronUpIcon
-                  className={`${
-                    open ? 'rotate-180 transform' : ''
-                  } h-5 w-5 text-purple-500`}
+                  className={`${open ? 'rotate-180 transform' : ''} h-5 w-5`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+              <Disclosure.Panel className="pb-4 text-sm text-left">
                 No.
               </Disclosure.Panel>
-            </>
+            </div>
           )}
         </Disclosure>
       </div>
