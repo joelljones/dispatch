6c933c8b7fa1ed3ec0efb2f5f812f62c02a96cd8
Joell Jones <25757151+joelljones@users.noreply.github.com>
2023-08-14 21:50:56 -0400

    redesigned services section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
